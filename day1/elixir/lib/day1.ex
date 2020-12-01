7c53cb5b1b522d5e844c6790c91f433bddaceb32
Warut Surapat <s.warut@gmai.com>
2020-12-02 00:07:19 +0900

    First commit.

---
+++ day1/elixir/lib/day1.ex
@@ -0,0 +1,18 @@
+defmodule Day1 do
+  @moduledoc """
+  Documentation for `Day1`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day1.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
