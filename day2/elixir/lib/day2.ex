83e408f8130b799eeaebe4e54c991acf427ab585
Warut Surapat <s.warut@gmai.com>
2020-12-02 23:23:57 +0900

    Finish day 2 part 2.

--- day2/elixir/lib/day2.ex
+++ day2/elixir/lib/day2.ex
@@ -6,10 +6,10 @@ defmodule Day2 do
   end
 
   def solve_part1() do
-    metas = get_input()
+    get_input()
       |> Enum.map(fn x -> parse(x) |> validate end)
       |> Enum.filter(fn x -> x end)
-    IO.puts metas |> Enum.count
+      |> Enum.count
   end
 
   def parse(text) do
@@ -19,5 +19,19 @@ defmodule Day2 do
   def validate(%{"min" => min, "max" => max, "char" => char, "text" => text}) do
     char_count = String.length(text) - String.length(text |> String.replace(char, ""))
     (char_count >= String.to_integer(min)) && (char_count <= String.to_integer(max))
+  end
+
+  def solve_part2() do
+    get_input()
+      |> Enum.map(fn x -> parse(x) |> validate_part2 end)
+      |> Enum.filter(fn x -> x end)
+      |> Enum.count
+  end
+
+  def validate_part2(%{"min" => min, "max" => max, "char" => char, "text" => text}) do
+    min = String.to_integer(min) - 1
+    max = String.to_integer(max) - 1
+    (String.at(text, min) == char) && (String.at(text, max) != char) ||
+      (String.at(text, min) != char) && (String.at(text, max) == char)
   end
 end
--- day2/elixir/test/day2_test.exs
+++ day2/elixir/test/day2_test.exs
@@ -10,7 +10,18 @@ defmodule Day2Test do
     assert Day2.validate(%{"min" => "3", "max" => "4", "char" =>"c", "text" => "cccc"}) == true
   end
 
-  test "validate() returns true if the text follows the false" do
+  test "validate() returns true if the text does not follow the rule" do
     assert Day2.validate(%{"min" => "6", "max" => "7", "char" =>"c", "text" => "cccc"}) == false
+  end
+
+  test "validate_part2() returns true if the text follows the rule" do
+    assert Day2.validate_part2(%{"min" => "1", "max" => "3", "char" =>"a", "text" => "abcde"}) == true
+  end
+
+  test "validate_part2() returns true if the text does not follow the rule" do
+    assert Day2.validate_part2(%{"min" => "1", "max" => "3", "char" =>"b", "text" => "cdefg"}) == false
+    assert Day2.validate_part2(%{"min" => "2", "max" => "9", "char" =>"c", "text" => "ccccccccc"}) == false
+    assert Day2.validate_part2(%{"min" => "4", "max" => "6", "char" =>"c", "text" => "cccxcjcwc"}) == false
+    assert Day2.validate_part2(%{"min" => "3", "max" => "8", "char" =>"p", "text" => "pppppppppppp"}) == false
   end
 end
