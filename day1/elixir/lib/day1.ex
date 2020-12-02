0424ee0e390a2d86ed71913d6d7b9c6a57654e11
Warut Surapat <s.warut@gmai.com>
2020-12-02 20:54:31 +0900

    Solve day 1 part 1.

--- day1/elixir/lib/day1.ex
+++ day1/elixir/lib/day1.ex
@@ -1,18 +1,22 @@
 defmodule Day1 do
-  @moduledoc """
-  Documentation for `Day1`.
-  """
+  def get_input do
+    {:ok, result} = File.read("input.txt")
 
-  @doc """
-  Hello world.
+    String.split(result, "\n", trim: true)
+    |> Enum.map(fn x -> String.to_integer(x) end)
+  end
 
-  ## Examples
+  def solve_part1 do
+    input = get_input()
+    pair = find_2020_sum_pair(input, %{})
+    {a, b} = pair
+    a * b
+  end
 
-      iex> Day1.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def find_2020_sum_pair([h | t], lookup) do
+    case lookup[2020 - h] do
+      nil -> find_2020_sum_pair(t, Map.put(lookup, h, 2020 - h))
+      _ -> {h, 2020 - h}
+    end
   end
 end
--- day1/elixir/test/day1_test.exs
+++ day1/elixir/test/day1_test.exs
@@ -2,7 +2,8 @@ defmodule Day1Test do
   use ExUnit.Case
   doctest Day1
 
-  test "greets the world" do
-    assert Day1.hello() == :world
+  test "find_2020_sum_pair returns a pair of number which their sum is 2020" do
+    input = [1, 2, 3, 2020, 2018, 10]
+    assert Day1.find_2020_sum_pair(input, %{}) == {2018, 2}
   end
 end
