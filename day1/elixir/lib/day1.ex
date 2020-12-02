2980a739dfac292824a2aa8ef1b33cfa59f0040c
Warut Surapat <s.warut@gmai.com>
2020-12-02 22:21:32 +0900

    Finish day 1 part 2.

--- day1/elixir/lib/day1.ex
+++ day1/elixir/lib/day1.ex
@@ -8,15 +8,36 @@ defmodule Day1 do
 
   def solve_part1 do
     input = get_input()
-    pair = find_2020_sum_pair(input, %{})
+    pair = find_sum_pair(2020, input, %{})
     {a, b} = pair
     a * b
   end
 
-  def find_2020_sum_pair([h | t], lookup) do
-    case lookup[2020 - h] do
-      nil -> find_2020_sum_pair(t, Map.put(lookup, h, 2020 - h))
-      _ -> {h, 2020 - h}
+  def find_sum_pair(sum, [h | t], lookup) do
+    case lookup[sum - h] do
+      nil -> find_sum_pair(sum, t, Map.put(lookup, h, sum - h))
+      _ -> {h, sum - h}
     end
   end
+
+  def find_sum_pair(_sum, [], _lookup) do
+    nil
+  end
+
+  def solve_part2 do
+    input = get_input()
+    result = find_three_numbers_with_sum(2020, input)
+    {a, b, c} = result
+    a * b * c
+  end
+
+  def find_three_numbers_with_sum(sum, [h | t]) do
+    next_sum = sum - h
+    pair = find_sum_pair(next_sum, t, %{})
+    case pair do
+      nil -> find_three_numbers_with_sum(sum, t)
+      {a, b} -> {a, b, h}
+    end
+  end
+
 end
--- day1/elixir/test/day1_test.exs
+++ day1/elixir/test/day1_test.exs
@@ -2,8 +2,13 @@ defmodule Day1Test do
   use ExUnit.Case
   doctest Day1
 
-  test "find_2020_sum_pair returns a pair of number which their sum is 2020" do
+  test "find_sum_pair returns a pair of number which their sum is 2020" do
     input = [1, 2, 3, 2020, 2018, 10]
-    assert Day1.find_2020_sum_pair(input, %{}) == {2018, 2}
+    assert Day1.find_sum_pair(2020, input, %{}) == {2018, 2}
+  end
+
+  test "find_three_numbers_with_sum returns three numbers which their sum is 2020" do
+    input = [1, 2, 3, 2020, 2018, 10, 2017]
+    assert Day1.find_three_numbers_with_sum(2020, input) == {2017, 2, 1}
   end
 end
