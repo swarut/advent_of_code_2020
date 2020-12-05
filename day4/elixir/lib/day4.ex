1b2b924532da9afb10ba8d2eeb9d8cabf941f091
Warut Surapat <s.warut@gmai.com>
2020-12-06 02:54:48 +0900

    Finish day4 part 2.

--- day4/elixir/lib/day4.ex
+++ day4/elixir/lib/day4.ex
@@ -7,36 +7,120 @@ defmodule Day4 do
     String.split(result, ["\n"])
   end
 
-  def solve_part1() do
+  def solve_part2() do
     input = get_input()
     count_valid_passport(input, MapSet.new(), 0)
   end
 
+  def validate_byr(%{"byr" => value}) do
+    case Regex.match?(~r/\d\d\d\d/, value) do
+      true ->
+        byr = String.to_integer(value)
+        (byr >= 1920) && (byr <= 2002)
+      false -> false
+    end
+  end
+
+  def validate_iyr(%{"iyr" => value}) do
+    case Regex.match?(~r/\d\d\d\d/, value) do
+      true ->
+        iyr = String.to_integer(value)
+        (iyr >= 2010) && (iyr <= 2020)
+      false -> false
+    end
+  end
+
+  def validate_eyr(%{"eyr" => value}) do
+    case Regex.match?(~r/\d\d\d\d/, value) do
+      true ->
+        eyr = String.to_integer(value)
+        (eyr >= 2020) && (eyr <= 2030)
+      false -> false
+    end
+  end
+
+  def validate_hgt(%{"hgt" => value}) do
+    case Regex.named_captures(~r/(?<height>\d+)(?<unit>cm|in)/, value) do
+      %{"height" => height, "unit" => "cm"} ->
+        h = String.to_integer(height)
+        (h >= 150) && (h <= 193)
+      %{"height" => height, "unit" => "in"} ->
+        h = String.to_integer(height)
+        (h >= 59) && (h <= 76)
+      _ -> false
+    end
+  end
+
+  def validate_hcl(%{"hcl" => value}) do
+    Regex.match?(~r/#[0-9a-f]{6}/, value)
+  end
+
+  def validate_ecl(%{"ecl" => value}) do
+    Regex.match?(~r/amb|blu|brn|gry|grn|hzl|oth/, value)
+  end
+
+  def validate_pid(%{"pid" => value}) do
+    (String.length(value) == 9) && Regex.match?(~r/\d{9}/, value)
+  end
+
+  def validate_passport(passport_memo) do
+    available_fields = MapSet.new(passport_memo |> Map.keys)
+    case MapSet.equal?(@required_fields, available_fields |> MapSet.delete("cid")) do
+      true -> true
+      false -> false
+    end
+
+    with true <- MapSet.equal?(@required_fields, available_fields |> MapSet.delete("cid")),
+      true <- validate_byr(passport_memo),
+      true <- validate_iyr(passport_memo),
+      true <- validate_eyr(passport_memo),
+      true <- validate_hgt(passport_memo),
+      true <- validate_hcl(passport_memo),
+      true <- validate_ecl(passport_memo),
+      true <- validate_pid(passport_memo)
+    do
+      true
+    else
+      false -> false
+    end
+  end
+
   def count_valid_passport([], passport_memo, count) do
-    case MapSet.equal?(@required_fields, passport_memo |> MapSet.delete("cid")) do
+    case validate_passport(passport_memo) do
       true -> count + 1
       false -> count
     end
+    # available_fields = MapSet.new(passport_memo |> Map.keys)
+    # case MapSet.equal?(@required_fields, available_fields |> MapSet.delete("cid")) do
+    #   true -> count + 1
+    #   false -> count
+    # end
   end
 
   def count_valid_passport(["" | t], passport_memo, count) do
-    # validate
-    case MapSet.equal?(@required_fields, passport_memo |> MapSet.delete("cid")) do
-      true -> count_valid_passport(t, MapSet.new(), count + 1)
-      false -> count_valid_passport(t, MapSet.new(), count)
+    case validate_passport(passport_memo) do
+      true -> count_valid_passport(t, %{}, count + 1)
+      false -> count_valid_passport(t, %{}, count)
     end
+
+    # validate
+    # IO.inspect(passport_memo)
+    # available_fields = MapSet.new(passport_memo |> Map.keys)
+    # case MapSet.equal?(@required_fields, available_fields |> MapSet.delete("cid")) do
+    #   true ->
+    #     count_valid_passport(t, %{}, count + 1)
+    #   false -> count_valid_passport(t, %{}, count)
+    # end
   end
 
-  def count_valid_passport([h | t], passport_memo = %MapSet{}, count) do
+  def count_valid_passport([h | t], passport_memo = %{}, count) do
     # build up
     tokens = h |> String.split(" ")
     passport_memo = tokens
       |> Enum.reduce(passport_memo, fn token, acc ->
-        key = ~r/\w\w\w:/
-          |> Regex.run(token)
-          |> List.first
-          |> String.slice(0, 3)
-        acc |> MapSet.put(key)
+        [_, key, value] = ~r/(?<key>\w\w\w):(?<value>.*)/ |> Regex.run(token)
+
+        acc |> Map.put(key, value)
       end)
     count_valid_passport(t, passport_memo, count)
   end
--- day4/elixir/test/day4_test.exs
+++ day4/elixir/test/day4_test.exs
@@ -2,7 +2,66 @@ defmodule Day4Test do
   use ExUnit.Case
   doctest Day4
 
-  test "greets the world" do
-    assert Day4.hello() == :world
+  test "validate_byr" do
+    assert Day4.validate_byr(%{"byr" => "1920"}) == true
+    assert Day4.validate_byr(%{"byr" => "2002"}) == true
+    assert Day4.validate_byr(%{"byr" => "1919"}) == false
+    assert Day4.validate_byr(%{"byr" => "2003"}) == false
+  end
+
+  test "validate_iyr" do
+    assert Day4.validate_iyr(%{"iyr" => "2010"}) == true
+    assert Day4.validate_iyr(%{"iyr" => "2020"}) == true
+    assert Day4.validate_iyr(%{"iyr" => "2009"}) == false
+    assert Day4.validate_iyr(%{"iyr" => "2021"}) == false
+  end
+
+  test "validate_eyr" do
+    assert Day4.validate_eyr(%{"eyr" => "2020"}) == true
+    assert Day4.validate_eyr(%{"eyr" => "2030"}) == true
+    assert Day4.validate_eyr(%{"eyr" => "2019"}) == false
+    assert Day4.validate_eyr(%{"eyr" => "2031"}) == false
+  end
+
+  test "validate_hgt" do
+    assert Day4.validate_hgt(%{"hgt" => "150cm"}) == true
+    assert Day4.validate_hgt(%{"hgt" => "193cm"}) == true
+    assert Day4.validate_hgt(%{"hgt" => "149cm"}) == false
+    assert Day4.validate_hgt(%{"hgt" => "194cm"}) == false
+
+    assert Day4.validate_hgt(%{"hgt" => "59in"}) == true
+    assert Day4.validate_hgt(%{"hgt" => "76in"}) == true
+    assert Day4.validate_hgt(%{"hgt" => "58in"}) == false
+    assert Day4.validate_hgt(%{"hgt" => "77in"}) == false
+  end
+
+  test "validate_hcl" do
+    assert Day4.validate_hcl(%{"hcl" => "#000000"}) == true
+    assert Day4.validate_hcl(%{"hcl" => "#999999"}) == true
+    assert Day4.validate_hcl(%{"hcl" => "#aaaaaa"}) == true
+    assert Day4.validate_hcl(%{"hcl" => "#ffffff"}) == true
+    assert Day4.validate_hcl(%{"hcl" => ""}) == false
+    assert Day4.validate_hcl(%{"hcl" => "000"}) == false
+    assert Day4.validate_hcl(%{"hcl" => "#000"}) == false
+    assert Day4.validate_hcl(%{"hcl" => "#AAA"}) == false
+    assert Day4.validate_hcl(%{"hcl" => "#zzz000"}) == false
+    assert Day4.validate_hcl(%{"hcl" => "#111abz"}) == false
+  end
+
+  test "validate_ecl" do
+    assert Day4.validate_ecl(%{"ecl" => "amb"}) == true
+    assert Day4.validate_ecl(%{"ecl" => "blu"}) == true
+    assert Day4.validate_ecl(%{"ecl" => "brn"}) == true
+    assert Day4.validate_ecl(%{"ecl" => "gry"}) == true
+    assert Day4.validate_ecl(%{"ecl" => "grn"}) == true
+    assert Day4.validate_ecl(%{"ecl" => "hzl"}) == true
+    assert Day4.validate_ecl(%{"ecl" => "oth"}) == true
+    assert Day4.validate_ecl(%{"ecl" => "abc"}) == false
+  end
+
+  test "validate_pid" do
+    assert Day4.validate_pid(%{"pid" => "111111111"}) == true
+    assert Day4.validate_pid(%{"pid" => "1111111111"}) == false
+    assert Day4.validate_pid(%{"pid" => "1"}) == false
   end
 end
